b5dcb23d80d342c04e5a4c0953ccd8d55035b1b8
Nickolas Reid <nickolaswdreid@gmail.com>
2013-10-16 19:10:36 -0300

    Added files.

---
+++ Nickolas Reid - Game of life/Nickolas Reid - Game of life/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Nickolas_Reid___Game_of_life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CellReality = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblGeneration = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.CellReality)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CellReality
+            // 
+            this.CellReality.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.CellReality.AllowUserToAddRows = false;
+            this.CellReality.AllowUserToDeleteRows = false;
+            this.CellReality.AllowUserToResizeColumns = false;
+            this.CellReality.AllowUserToResizeRows = false;
+            this.CellReality.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.CellReality.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.CellReality.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.CellReality.ColumnHeadersHeight = 10;
+            this.CellReality.ColumnHeadersVisible = false;
+            this.CellReality.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CellReality.Enabled = false;
+            this.CellReality.EnableHeadersVisualStyles = false;
+            this.CellReality.Location = new System.Drawing.Point(0, 24);
+            this.CellReality.Name = "CellReality";
+            this.CellReality.ReadOnly = true;
+            this.CellReality.RowHeadersVisible = false;
+            this.CellReality.RowHeadersWidth = 10;
+            this.CellReality.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.CellReality.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.CellReality.Size = new System.Drawing.Size(442, 465);
+            this.CellReality.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(442, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblGeneration});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 467);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(442, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblGeneration
+            // 
+            this.lblGeneration.Name = "lblGeneration";
+            this.lblGeneration.Size = new System.Drawing.Size(78, 17);
+            this.lblGeneration.Text = "Generations: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 489);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.CellReality);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Game of Life - Nickolas reid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CellReality)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CellReality;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblGeneration;
+    }
+}
+
---
+++ Nickolas Reid - Game of life/Nickolas Reid - Game of life/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Nickolas_Reid___Game_of_life
+{
+    public partial class Form1 : Form
+    {
+        
+        // MAkes random numbers through out the program
+        Random rand = new Random();
+        // The "years" a cell will go through.
+        static int CellGenerations = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Creates the grid for the cells to live on.
+            CreateBoard();
+            // Populates cells
+            MakeCell();
+            // gets rid of a cell being auto selected.
+            CellReality.ClearSelection();
+            // Starts the cells life scale.
+            timer1.Start();
+            
+        }
+        private void CreateBoard()
+        {
+            // creates the columns for the cells.
+            for (int i = 0; i < 20; i++)
+            {
+                CellReality.Columns.Add(new DataGridViewColumn() { CellTemplate = new DataGridViewTextBoxCell() });
+                DataGridViewColumn column = CellReality.Columns[i];
+                column.Width = 20;
+                CellReality.Rows.Add();
+            }
+            // Creates the black frame or "walls" for the cells to live in.
+            for (int i = 0; i < 20; i++)
+            {
+                CellReality.Rows[0].Cells[i].Style.BackColor = Color.Black;
+                CellReality.Rows[19].Cells[i].Style.BackColor = Color.Black;
+                CellReality.Rows[i].Cells[0].Style.BackColor = Color.Black;
+                CellReality.Rows[i].Cells[19].Style.BackColor = Color.Black;
+            }
+            
+        }
+        private void MakeCell()
+        {      
+            // Using the random generator I create between 1 and 400 cells on the board.
+            for (int i = 0; i < rand.Next(1,400); i++)
+            {
+                CellReality.Rows[rand.Next(1, 19)].Cells[rand.Next(1, 19)].Style.BackColor = Color.PowderBlue;
+            }
+        }
+
+        private void CheckBoard()
+        {
+            // Checks to see if there is any life at all on the board, it's senseless to continue
+            // generations if there is not.
+             bool life = false;
+            // Going through the cells to check for life.
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (CellReality.Rows[i].Cells[j].Style.BackColor == Color.PowderBlue)
+                    {
+                        life = true;
+                    }
+                        // Calls check for life to do an evaluation to see if a cell lives or dies.
+                        CheckForLife(i, j);                 
+                }
+            }
+            // All life has died.
+            if(!life){
+                // Stop the generations.
+                timer1.Stop();
+                MessageBox.Show("All life has died");
+            }
+        }
+        private void CheckForLife(int row, int col)
+        {
+            // Counter based on the rules of living or dying.
+            int counter = 0;
+            // Checks if the cells can grow within bounds.
+            if(row >= 1 && col >= 1 && row <= 18 && col <= 18){
+                // Checks the positioning around the cell. Like this:
+                // x|x|x
+                // x| |x
+                // x|x|x
+                if (CellReality.Rows[row - 1].Cells[col].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row + 1].Cells[col].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row].Cells[col - 1].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row].Cells[col + 1].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row - 1].Cells[col - 1].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row - 1].Cells[col + 1].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row + 1].Cells[col - 1].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                if (CellReality.Rows[row + 1].Cells[col + 1].Style.BackColor == Color.PowderBlue)
+                {
+                    counter++;
+                }
+                // Go to the method to determine life and death of a cell.
+                LifeOrDie(counter, row, col);
+            }
+
+        }
+        private void LifeOrDie(int lives,int row,int col)
+        {
+            
+            if (lives > 3)
+            {
+                CellReality.Rows[row].Cells[col].Style.BackColor = Color.White;
+            }
+            else if (lives == 3)
+            {
+                CellReality.Rows[row].Cells[col].Style.BackColor = Color.PowderBlue;
+
+            }
+            else if (lives == 2 || lives == 3) {
+              //Experimental
+                // CellReality.Rows[row].Cells[col].Style.BackColor = Color.Purple;
+            }
+
+            else
+            {
+                CellReality.Rows[row].Cells[col].Style.BackColor = Color.White;
+            } 
+        }
+        private void StartGame()
+        {       
+                // Checks the board for life.
+                CheckBoard();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Start the game.
+            StartGame();
+            // Year has passed for cells.
+            CellGenerations++;
+            // Show generation at bottom of the screen.
+            lblGeneration.Text = "Generations: " + CellGenerations.ToString();
+
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Set all the cells back to white.
+            foreach (DataGridViewRow row in this.CellReality.Rows)
+            {
+                foreach (DataGridViewCell Cell in row.Cells)
+                {
+                    Cell.Style.BackColor = Color.White;
+                }
+            }
+            // Creates the black frame or "walls" for the cells to live in.
+            for (int i = 0; i < 20; i++)
+            {
+                CellReality.Rows[0].Cells[i].Style.BackColor = Color.Black;
+                CellReality.Rows[19].Cells[i].Style.BackColor = Color.Black;
+                CellReality.Rows[i].Cells[0].Style.BackColor = Color.Black;
+                CellReality.Rows[i].Cells[19].Style.BackColor = Color.Black;
+            }
+            // seed cells
+            MakeCell();
+            // make sure nothing is selected
+            CellReality.ClearSelection();
+            //year zero
+            CellGenerations = 0;
+            // show year zero
+            lblGeneration.Text = "Generations: " + CellGenerations.ToString();
+            // Bring the cells to life.
+            timer1.Start();
+
+            
+        }
+    }
+}
